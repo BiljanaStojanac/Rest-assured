88c1794635a88df13247c5cb5ac36dc56da53828
Biljana Stojanac <bstojanac@sixsentix.internal>
2020-11-30 09:21:04 +0100

    Initial commit

---
+++ src/test/java/Base.java
@@ -0,0 +1,12 @@
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers.*;
+
+public class Base {
+
+
+    public static void main(String args[])
+    {
+        RestAssured.baseURI =
+
+    }
+}
